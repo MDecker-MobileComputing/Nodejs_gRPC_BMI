56f8b773798ddbab334ecf5842b7cbe7bfebc26f
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-02-16 16:54:52 +0100

    BMI-[Client|Server].js in Unterordner ClientServer-ProtoBased/ verschoben

--- ClientServer-ProtoBased/BMI-Server.js
+++ ClientServer-ProtoBased/BMI-Server.js
@@ -2,7 +2,7 @@ const grpc        = require( "@grpc/grpc-js"      );
 const protoLoader = require( "@grpc/proto-loader" );
 const path        = require( "path"               );
 
-const PROTO_PATH = path.join( __dirname, "bmi_berechnung.proto" );
+const PROTO_PATH = path.join( __dirname, "../bmi_berechnung.proto" );
 const packageDefinition = 
         protoLoader.loadSync( PROTO_PATH, 
                              {
