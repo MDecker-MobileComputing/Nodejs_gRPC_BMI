bd5c40d881d80ae03b79a6f0d7dd542b57f42bd7
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-02-16 16:34:02 +0100

    Update BMI-Client.js

--- BMI-Client.js
+++ BMI-Client.js
@@ -1,34 +1,44 @@
-const grpc = require('@grpc/grpc-js');
-const protoLoader = require('@grpc/proto-loader');
-const path = require('path');
+const grpc        = require( "@grpc/grpc-js"      );
+const protoLoader = require( "@grpc/proto-loader" );
+const path        = require( "path"               );
 
-const PROTO_PATH = path.join(__dirname, 'bmi_berechnung.proto');
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true
-});
-const bmiProto = grpc.loadPackageDefinition(packageDefinition).bmi;
+const PROTO_PATH = path.join( __dirname, "bmi_berechnung.proto" );
+const packageDefinition = 
+        protoLoader.loadSync( PROTO_PATH, 
+                              { keepCase: true,
+                                longs: String,
+                                enums: String,
+                                defaults: true,
+                                oneofs: true
+                              }
+                            );
+const bmiProto = grpc.loadPackageDefinition( packageDefinition ).bmi;
 
 function main() {
-  const client = new bmiProto.BmiDienst('localhost:50051', grpc.credentials.createInsecure());
+
+  const client = new bmiProto.BmiDienst( "localhost:50051", 
+                                         grpc.credentials.createInsecure()
+                                        );
 
   const request = {
-    gewicht_kg: 70,
-    koerpergroesse_cm: 175
+            gewicht_kg: 68,
+            koerpergroesse_cm: 195
   };
 
-  client.BerechneBmi(request, (error, response) => {
-    if (error) {
-      console.error('Fehler:', error);
+  client.BerechneBmi( request, ( error, response ) => {
+
+    if ( error ) {
+
+      console.error( "Fehler:", error);
+
     } else {
 
       const bmiWert           = response.bmi_wert;
       const bmiInterpretation = response.bmi_interpretation;
 
-      console.log(`\nBMI-Wert: ${bmiWert} (${bmiInterpretation})\n`);
+      const bmiWertGerundet = Math.round( bmiWert * 10 ) / 10;
+
+      console.log(`\nBMI-Wert: ${bmiWertGerundet} (${bmiInterpretation})\n`);
     }
   });
 }
