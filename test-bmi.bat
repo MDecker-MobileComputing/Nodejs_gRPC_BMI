78199f07531b49f2d57b3ab8f96139226dcf31ad
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-03-02 16:41:20 +0100

    Update test-bmi.bat

--- test-bmi.bat
+++ test-bmi.bat
@@ -2,8 +2,12 @@
 @REM Programm "grpcurl.exe" muss im aktuellen Verzeichnis oder im PATH gefunden werden.
 @REM Für Download siehe https://repology.org/project/grpcurl/information , Abschnitt "Download"
 
-grpcurl -plaintext ^
-        -d "{\"gewicht_kg\": 70, \"koerpergroesse_cm\": 175}" ^
-        -import-path . ^
-        -proto bmi_berechnung.proto ^
-        localhost:50051 bmi.BmiDienst/BerechneBmi
+@echo.
+
+@grpcurl -plaintext ^
+         -d "{\"gewicht_kg\": 70, \"koerpergroesse_cm\": 175}" ^
+         -import-path . ^
+         -proto bmi_berechnung.proto ^
+         localhost:50051 bmi.BmiDienst/BerechneBmi
+
+@echo.
